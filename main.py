51c43f756069317007be6e37093c81a32585ed4a
airrey-stardust <51347085+airrey-stardust@users.noreply.github.com>
2019-07-22 17:35:21 -0500

    Add files via upload
    done

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:root@localhost:3306/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:3306/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 ### changed to port 3306 ###
